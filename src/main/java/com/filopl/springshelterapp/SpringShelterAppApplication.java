ae7184e8294c4b51cc3c73aa5fc040ea47b6cc92
FiloPL <tzegarlicki@gmail.com>
2022-03-10 22:27:44 +0100

    Initial commit

---
+++ src/main/java/com/filopl/springshelterapp/SpringShelterAppApplication.java
@@ -0,0 +1,13 @@
+package com.filopl.springshelterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShelterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShelterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filopl/springshelterapp/SpringShelterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filopl.springshelterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShelterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
